64ddfd77a19e1443fea20faab39b49b529e814c8
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-11-05 13:20:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3236797647-636d0",
+  "appId": "1:609621503421:web:3e4635fa79a925c005b6d4",
+  "apiKey": "AIzaSyDX9X2NfWsRNLZQa4SY00Pf6oQlbDg_elI",
+  "authDomain": "studio-3236797647-636d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "609621503421"
+};
