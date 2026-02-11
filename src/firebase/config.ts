99199e4c62afbc47f4ef1d5f31182a4aaeb99a89
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2026-02-11 21:17:31 +0530

    Replace Firebase appId and apiKey with placeholders
    Updated Firebase configuration with placeholder values for appId and apiKey.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3236797647-636d0",
-  "appId": "1:609621503421:web:3e4635fa79a925c005b6d4",
-  "apiKey": "AIzaSyDX9X2NfWsRNLZQa4SY00Pf6oQlbDg_elI",
+  "appId": "Your APIID",
+  "apiKey": "Your APIKey",
   "authDomain": "studio-3236797647-636d0.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "609621503421"
